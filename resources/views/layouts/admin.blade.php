cf0cfbbf4734615372e89cf3998543dec170b814
tajima haruna <power_yellow.28@docomo.ne.jp>
2019-11-28 09:48:45 +0000

    5th comit

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <title>@yield('title')</title>
+        <script src="{{ secure_asset('js/app.js') }}"defer></script>
+        
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" date-toggle="collapse" date-target="#navbarSupportedContent" aria-controls="naverSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navber-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navber-collapse" id="navberSupportContent">
+                        <ul class="navber-nav mr-auto">
+                            
+                        </ul> 
+                        <ul class="nuvber-nav ml-auto">
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </body>
+</html>
