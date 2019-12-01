811312833cc885bb744ccedbfe4af770c7bba461
tajima haruna <power_yellow.28@docomo.ne.jp>
2019-12-01 06:57:24 +0000

    PHP/Laravel 13 ニュース投稿画面を作成しよう

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -11,4 +11,7 @@ class NewsController extends Controller
     public function add(){
         return view('admin.news.create');
     }
+    public function create(Request $request){
+        return redirect('admin/news/create');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create');
 });
-
-/* 17. PHP/Laravel 09 Routingについて理解する
-
-3.「http://XXXXXX.jp/XXX というアクセスが来たときに、 
-AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。*/
 
 Route::group(['prefix' => 'XXX'], function() {
     Route::get('AAAController@bbb');
@@ -30,15 +26,12 @@ Route::group(['prefix' => 'XXX'], function() {
 
 Route::get('XXX', 'bbb\AAAController@add');
 
-/* 
-4.【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを
-追加しました。web.phpを編集して、admin/profile/create にアクセスしたら 
-ProfileController の add Action に、admin/profile/edit にアクセスしたら 
-ProfileController の edit Action に割り当てるように設定してください。 */
 
-Route::group(['prefix' => 'admin'],function(){
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'],function(){
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profole/create','Admin\ProfileController@create');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile/edit','Admin\ProfileController@update');
 });
 
 Auth::routes();
